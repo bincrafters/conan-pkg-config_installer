74bb27711b5ff425b4340816ca61a3a6fe8e3770
SSE4 <tomskside@gmail.com>
2019-02-11 00:32:23 +0700

    - respect MINGW_CONFIGURATION (ugly)
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -9,5 +9,18 @@ if __name__ == "__main__":
 
     arch = os.environ["ARCH"]
     builder = build_template_installer.get_builder()
-    builder.add({"os" : build_shared.get_os(), "arch_build" : arch, "arch": arch}, {}, {}, {})
+    settings = {"os": build_shared.get_os(), "arch_build": arch, "arch": arch}
+    if "MINGW_CONFIGURATIONS" in os.environ:
+        configs = os.environ["MINGW_CONFIGURATIONS"]
+        for config in configs.split(","):
+            tokens = config.strip().split('@')
+            settings["compiler"] = "gcc"
+            settings["compiler.version"] = tokens[0]
+            settings["arch"] = tokens[1]
+            settings["compiler.exception"] = tokens[2]
+            settings["compiler.threads"] = tokens[3]
+            settings["compiler.libcxx"] = "libstdc++11"
+            builder.add(settings, {}, {}, {})
+    else:
+        builder.add(settings, {}, {}, {})
     builder.run()
